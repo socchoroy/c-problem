a0f8582f5a2fc7b522cb105645218bd8ea7ba7a6
soccho roy <93141362+socchoroy@users.noreply.github.com>
2022-06-29 06:07:26 +0600

    linear-search-for-charecter-

--- linear-search-for-charecter.c
+++ linear-search-for-charecter.c
@@ -1,44 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define size 100
-int available(char *word,char ch);
+int available(char *word, char ch);
 int main()
 {
- 
-    char ch,thing[size];
-    
-    int have;
-    printf("Please Enter a string : ");
-    gets(thing);
-    printf("Please Enter which charecter you want to search : ");
-    scanf("%c",&ch);
-    have=available(thing,ch);
-    if(have==1)
-    {
-        printf("we found the charetcer!");
-    }
-    else
-    {
-        printf("we didn't found it!");
-    }
 
-}
-int available(char *word,char ch)
-{
-    int present=0,count=0,space=0;
-  while(*word!=NULL)
+  char ch, thing[size];
+
+  int have;
+  printf("Please Enter a string : ");
+  gets(thing);
+  printf("Please Enter which charecter you want to search : ");
+  scanf("%c", &ch);
+  have = available(thing, ch);
+  if (have == 1)
   {
-        count++;
-        if(ch==*word)
-        {
-          present=1;
-          break;
-         
-        }
-        word++;
+    printf("we found the charetcer!");
+  }
+  else
+  {
+    printf("we didn't found it!");
+  }
+}
+int available(char *word, char ch)
+{
+  int present = 0, count = 0, space = 0;
+  while (*word != NULL)
+  {
+    count++;
+    if (ch == *word)
+    {
+      present = 1;
+      break;
     }
-  printf("\nfound in %d position\n",count);
-    return present;
-    
+    word++;
+  }
+  printf("\nfound in %d position\n", count);
+  return present;
 }
